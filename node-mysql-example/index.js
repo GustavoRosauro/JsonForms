9df1ea70fdf321b344d34a029f7aa44a1e541a19
Ubuntu <azureuser@vFormsVM.1iuvaa4kzmdu1ffypoqpsxp4mg.fx.internal.cloudapp.net>
2024-06-27 13:25:09 +0000

    updated

--- node-mysql-example/index.js
+++ node-mysql-example/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app.use(bodyParser.json());
 
@@ -36,6 +36,20 @@ app.get('/data', (req,res) =>{
     })
 })
 
-app.listen(port, () => {
-    console.log(`port ${port}`)
+app.get('/data/ui', (req,res)=>{
+   const sql = 'select * from uischema_table';
+   db.query(sql, (err, results) => {
+       if(err){
+	  return res.status(500).send(err);
+       }
+       const data = results.map(row => ({
+	    id: row.id, 
+	    data: JSON.parse(row.data)
+       }));
+       res.json(data);
+   })
+})
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`0.0.0.0 port ${port}`)
 })
