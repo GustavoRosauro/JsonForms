dfd3878e28e3f5ca992485fa81273795ad62a687
Gustavo Rosauro <gustavo.rosauro@hitachids.com>
2024-06-27 11:54:11 +0100

    first version

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+export const Header: FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src="./logo.jpg" className="App-logo" alt="logo" />
+    </header>
+  </div>
+);
